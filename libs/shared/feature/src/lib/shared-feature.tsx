f62222d3dce84faf719cc1ca77a0b48e3783e567
sumit-pandey <sumit_pandey@outlook.com>
2021-06-25 01:45:04 +0530

    added changes

--- libs/shared/feature/src/lib/shared-feature.tsx
+++ libs/shared/feature/src/lib/shared-feature.tsx
@@ -10,7 +10,7 @@ const StyledSharedFeature = styled.div`
 export function SharedFeature(props: SharedFeatureProps) {
   return (
     <StyledSharedFeature>
-      <h1>Another shared comp | comp</h1>
+      <h1>Another shared comp new</h1>
     </StyledSharedFeature>
   );
 }
